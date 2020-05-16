f0fb664d2ce5a50905c725f3405930101af37d3d
sai_3031 <saiprasanth.raghuraman@reverieinc.com>
2020-05-16 12:00:39 +0530

    added tts script

---
+++ tts.py
@@ -0,0 +1,29 @@
+import azure.cognitiveservices.speech as speechsdk
+
+# Replace with your own subscription key and region identifier from here: https://aka.ms/speech/sdkregion
+speech_key, service_region = "1553bca817174d4592e527c8af58d4e2", "eastus"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+# Creates an audio configuration that points to an audio file.
+# Replace with your own audio filename.
+audio_filename = "helloworld.wav"
+audio_output = speechsdk.audio.AudioOutputConfig(filename=audio_filename)
+
+# Creates a synthesizer with the given settings
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_output)
+
+# Synthesizes the text to speech.
+# Replace with your own text.
+text = "Hello world!"
+result = speech_synthesizer.speak_text_async(text).get()
+
+# Checks result.
+if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized to [{}] for text [{}]".format(audio_filename, text))
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+    print("Did you update the subscription info?")
