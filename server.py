e5257291debba0998cebc943daef93d6fa148f38
sai_3031 <saiprasanth.raghuraman@reverieinc.com>
2020-05-23 23:06:28 +0530

    added css file

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET"])
 def home():
-    os.system("rm voice.wav")
+    os.system("rm static/voice.wav")
     return render_template("index.html")
 
 @app.route("/audiofile")
